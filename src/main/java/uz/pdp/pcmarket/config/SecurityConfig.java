52a5df64a975f3a876296659d16846c9802c2002
Madinakh <shahzadenishonova@gmail.com>
2021-08-12 23:27:49 +0500

    PC MArket security

--- src/main/java/uz/pdp/pcmarket/config/SecurityConfig.java
+++ src/main/java/uz/pdp/pcmarket/config/SecurityConfig.java
@@ -1,10 +1,46 @@
 package uz.pdp.pcmarket.config;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/api/**").hasAnyRole("MODERATOR","SUPER_ADMIN","OPERATOR")
+                .antMatchers(HttpMethod.POST,"/api/**").hasAnyRole("MODERATOR","SUPER_ADMIN")
+                .antMatchers(HttpMethod.PUT,"/api/**").hasAnyRole("MODERATOR","SUPER_ADMIN")
+                .antMatchers("/api/**").hasRole("SUPER_ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("super_admin").password(passwordEncoder().encode("super_admin")).roles("SUPER_ADMIN")
+                .and()
+                .withUser("moderator").password(passwordEncoder().encode("moderator")).roles("MODERATOR")
+                .and()
+                .withUser("operator").password(passwordEncoder().encode("operator")).roles("OPERATOR");
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
